613cc517a6443b1195c8bb6207fa1c75eb10a36e
elieljohn <ejmalabar88@gmail.com>
2024-06-05 16:35:25 +0100

    RxJS Operators
    1. Add a 'pipe' to the 'from' function and test 'map', 'tap', and 'take' operators
    app.component.ts: line 18-34

--- rxjs-test/src/app/app.component.ts
+++ rxjs-test/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { of, from } from 'rxjs';
+import { of, from, map, tap, take } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,18 +13,35 @@ export class AppComponent implements OnInit{
   title = 'rxjs-test';
 
   ngOnInit(): void {
-      of(2, 4, 6, 8).subscribe(item => console.log(item));
+      // of(2, 4, 6, 8).subscribe(item => console.log(item));
 
-      from([20, 15, 10, 51]).subscribe({
+      from([20, 15, 10, 5]).pipe(
+        // Tap is used to perform side-effects for notifications from the source observable
+        tap(item => console.log(`emitted item .... ${item}`)),
+
+        // Map applies a function to each item and returns the value as a new observable
+        map(item => item * 2),
+        map(item => item - 10),
+        map(item => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+
+        // Take emits only the specified number of items
+        take(3)
+      )
+      .subscribe({
         next: (item) => console.log(`resulting item.. ${item}`),
         error: (err) => console.log(`error occurred ${err}`),
         complete: () => console.log('complete')
       })
 
-      of('Apple1', 'Apple2', 'Apple3').subscribe({
-        next: (apple) => console.log(`Apple emitted ${apple}`),
-        error: (err) => console.log(`Error occurred ${err}`),
-        complete: () => console.log('No more apples, go home')
-      })
+      // of('Apple1', 'Apple2', 'Apple3').subscribe({
+      //   next: (apple) => console.log(`Apple emitted ${apple}`),
+      //   error: (err) => console.log(`Error occurred ${err}`),
+      //   complete: () => console.log('No more apples, go home')
+      // })
   }
 }
