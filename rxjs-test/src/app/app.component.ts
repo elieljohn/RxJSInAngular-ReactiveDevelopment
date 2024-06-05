b5daeebb5cfbcd6aa6afca6554440bd36ecc8209
elieljohn <ejmalabar88@gmail.com>
2024-06-05 15:10:08 +0100

    Creation Functions
    1. Import { of, from }
    app.component.ts: line 3
    
    2. Implement OnInit with sample of 'of' and 'from'
    app.component.ts: line 12-29

--- rxjs-test/src/app/app.component.ts
+++ rxjs-test/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { of, from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'rxjs-test';
+
+  ngOnInit(): void {
+      of(2, 4, 6, 8).subscribe(item => console.log(item));
+
+      from([20, 15, 10, 51]).subscribe({
+        next: (item) => console.log(`resulting item.. ${item}`),
+        error: (err) => console.log(`error occurred ${err}`),
+        complete: () => console.log('complete')
+      })
+
+      of('Apple1', 'Apple2', 'Apple3').subscribe({
+        next: (apple) => console.log(`Apple emitted ${apple}`),
+        error: (err) => console.log(`Error occurred ${err}`),
+        complete: () => console.log('No more apples, go home')
+      })
+  }
 }
