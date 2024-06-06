d14e39862304ce10d7a84130a1353b47deaf2fb5
elieljohn <ejmalabar88@gmail.com>
2024-06-06 17:11:45 +0100

    Improving Change Detection
    1. Set ChangeDetectionStrategy to OnPush to minimize change detection cycles and improve performance
    product-list.component.ts: line 11

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { EMPTY, Observable, catchError } from 'rxjs';
 
 import { ProductCategory } from '../product-categories/product-category';
@@ -7,7 +7,8 @@ import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush // Component is checked only when (1) @Input properties change, (2) event emits, or (3) a bound Observable emits
 })
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
